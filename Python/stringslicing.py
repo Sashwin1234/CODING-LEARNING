1c5844df1372343dcbf52f77791188d06131bd86
Sashwin1234 <savageplayer364@gmail.com>
2023-03-20 21:46:23 +0530

    h

---
+++ Python/stringslicing.py
@@ -0,0 +1,8 @@
+#string slicing
+#negetive
+a="my name is sashwin "
+print (a[0:-4])
+#it will cansil the negetive side
+b="my name is sashwin"
+print (b[0:5])
+#it will print the words between the numbers
