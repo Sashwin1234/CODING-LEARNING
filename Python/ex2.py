1162a4d99f3dbb8e63ca91c9e2b988d508421d45
Sashwin1234 <savageplayer364@gmail.com>
2023-03-27 20:26:22 +0530

    h

--- Python/ex2.py
+++ Python/ex2.py
@@ -1,11 +1,29 @@
 import time
 hour = int(time.strftime('%H'))
 name=input("Please enter your name : ")
-if hour >0 and hour <=11 :
-    print ("Good Morning "+name)
-elif hour >=12 and hour <=16 :
-    print ("Good Afternoon "+name)
-else :
-    print("Good Evening "+name)
+gender = input("Please enter your gender male/female/other : ")
+if gender=="male" :
+ if hour >0 and hour <=11 :
+    print ("Good Morning Sir")
+ elif hour >=12 and hour <=16 :
+    print ("Good Afternoon Sir")
+ else :
+    print("Good Evening Sir")
+elif gender=="female":
+ if hour >0 and hour <=11 :
+    print ("Good Morning Mam")
+ elif hour >=12 and hour <=16 :
+    print ("Good Afternoon Mam")
+ else :
+    print("Good Evening Mam")
+else:
+ if hour >0 and hour <=11 :
+    print ("Good Morning ")
+ elif hour >=12 and hour <=16 :
+    print ("Good Afternoon ")
+ else :
+    print("Good Evening ")
+   
+   
 
 
