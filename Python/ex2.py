4308f1163616a4ac0f09bdc3cfbc3f948ebe81d4
Sashwin1234 <savageplayer364@gmail.com>
2023-04-05 07:02:09 +0530

    45

--- Python/ex2.py
+++ Python/ex2.py
@@ -3,17 +3,18 @@ import time
 hour = int(time.strftime('%H'))
 name=input("Please enter your name : ")
 gender = input("Please enter your gender male/female/other : ")
-if gender=="male" :
+gd= (gender.lower)
+if gd=="male" :
  if hour >0 and hour <=11 :
     print ("Good Morning Sir")
  elif hour >=12 and hour <=16 :
     print ("Good Afternoon Sir")
  else :
     print("Good Evening Sir")
-elif gender=="female":
+elif gd=="female":
  if hour >0 and hour <=11 :
     print ("Good Morning Mam")
- elif hour >=12 and hour <=16 :
+ elif hour >=12 and hour <=17 :
     print ("Good Afternoon Mam")
  else :
     print("Good Evening Mam")
---
+++ Python/loop.py
@@ -0,0 +1,17 @@
+name = "sashwin"
+for i in name :
+    print (i)
+    if i=="s" :
+     print ("good")
+
+#forlist
+a= ["burger","pizza","hello","word"]
+for b in a :
+   print (b)
+#for single letter  in a  word
+   for n in b:
+      print (n)
+#numbers
+for k in range(1,46):
+    print (k)
+ #it will print 1 to 10 because python print numbers between  the given numbers
